70950605b4e1f63dcbac062d547bca964c941f4c
Tkadla <tkadlecek.j@gmail.com>
2013-02-22 19:06:23 +0100

    Init commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Tkadla
+ *
+ * Created on 22. únor 2013, 18:57
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int tst = 0; 
+    return 0;
+}
+
